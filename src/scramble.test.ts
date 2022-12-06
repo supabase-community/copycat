212c16fc780ded361c4b4ad76960eb2a52bc5849
Justin van der Merwe <justnvdm@gmail.com>
2022-12-06 16:41:57 +0200

    Update tests after using fictional#faster

--- src/scramble.test.ts
+++ src/scramble.test.ts
@@ -2,34 +2,34 @@ import { copycat } from '.'
 
 test('scrambling', () => {
   expect(copycat.scramble('the DOG ate the cheese!')).toMatchInlineSnapshot(
-    `"nmu MIZ pvi cil mnmljaq"`
+    `"vff FQS qsz hnn tkupbcN"`
   )
 
   expect(copycat.scramble('99 red balloons')).toMatchInlineSnapshot(
-    `"33 wiv wpgvcttu"`
+    `"51 qop vytjofmj"`
   )
 })
 
 test('preserving chars', () => {
   expect(
     copycat.scramble('foo@bar.org', { preserve: ['@', '.'] })
-  ).toMatchInlineSnapshot(`"mrv@cky.ayc"`)
+  ).toMatchInlineSnapshot(`"ole@qpj.sre"`)
 })
 
 test('special chars', () => {
   expect(
     copycat.scramble('foo-bar_baz+quux@corge.org', { preserve: ['@', '.'] })
-  ).toMatchInlineSnapshot(`"jguPbkr6rkhhgjcz@btowy.smz"`)
+  ).toMatchInlineSnapshot(`"sdcXbbzwzrt0boos@vvlwz.jpm"`)
 })
 
 test('numbers', () => {
-  expect(copycat.scramble(999999999)).toMatchInlineSnapshot(`682197452`)
-  expect(copycat.scramble(782364.902374)).toMatchInlineSnapshot(`387998.531441`)
+  expect(copycat.scramble(999999999)).toMatchInlineSnapshot(`299349325`)
+  expect(copycat.scramble(782364.902374)).toMatchInlineSnapshot(`736762.255151`)
 })
 
 test('dates', () => {
   expect(copycat.scramble(new Date(89723948723948))).toMatchInlineSnapshot(
-    `7754-02-02T15:03:19.862Z`
+    `6575-08-08T16:04:54.156Z`
   )
 })
 
@@ -56,8 +56,8 @@ test('nested values', () => {
     Object {
       "a": Array [
         Object {
-          "b": 19,
-          "c": "ebp",
+          "b": 54,
+          "c": "air",
         },
       ],
     }
