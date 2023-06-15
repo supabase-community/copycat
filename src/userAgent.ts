b5114e8bb670bccb97cb677c2c849230642f690a
Carel de Waal <careldewaal@gmail.com>
2023-06-15 15:52:09 +0200

    Reduce the chance of generating duplicate userAgent responses

--- src/userAgent.ts
+++ src/userAgent.ts
@@ -3,7 +3,12 @@ import { oneOf } from 'fictional'
 import { Input } from './types'
 
 import { userAgentData as data } from './userAgentData'
+import { int, times } from './copycat'
 
 const maker = oneOf(data)
 
-export const userAgent = (input: Input): string => maker(input)
+export const userAgent = (input: Input): string => {
+  const str = maker(input)
+  const version = times(input, 3, int.options({ max: 9999 })).join('.')
+  return str.replace(/\{\{version\}\}/g, version)
+}
