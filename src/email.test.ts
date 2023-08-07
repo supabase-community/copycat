9a20539efe78a756194aca1de9b770c180ea29b1
Justin van der Merwe <justnvdm@gmail.com>
2023-08-07 12:06:00 +0200

    feat: Support domain option for copycat.email() (#44)

--- src/email.test.ts
+++ src/email.test.ts
@@ -5,3 +5,9 @@ import { checkGeneratedValue } from './testutils'
 test('generating fake emails', () => {
   checkGeneratedValue(isEmail, copycat.email)
 })
+
+test('custom domains', () => {
+  expect(copycat.email('foo', { domain: 'acme.org' }).split('@')[1]).toEqual(
+    'acme.org'
+  )
+})
