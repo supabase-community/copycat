2d7b95d2c580b7aad025c7aab00394b427eae836
Justin van der Merwe <justnvdm@gmail.com>
2023-10-04 21:35:18 +0200

    bench serializers

--- scripts/bench-serialize-optimization.js
+++ scripts/bench-serialize-optimization.js
@@ -2,29 +2,59 @@ const b = require('benny');
 const { copycat, fictional } = require('../dist')
 const nestedSample = require('./samples/nested')
 
-b.suite(
-  `copycat.scramble()`,
-  b.add('before', () => {
-    process.env.AFTER = false
-    copycat.scramble(nestedSample)
-  }),
-  b.add('after', () => {
-    process.env.AFTER = true
-    copycat.scramble(nestedSample)
-  }),
-  complete()
-)
+const serializerNames = Object.keys(global.__bench_serializers)
+
+;(() => {
+  const inputsIterator = generateWords(100_000)
+  let input = inputsIterator.next().value
+
+  b.suite(
+    `copycat.scramble(): 100_000 words`,
+    ...serializerNames.map(name => b.add(name, () => {
+      global.__bench_serializer = name
+      copycat.scramble(input)
+    })),
+    b.cycle(() => {
+      input = inputsIterator.next().value
+    }),
+    complete()
+  )
+})()
+
+;(() => {
+  const inputsIterator = generateWords(100_000)
+  let input = inputsIterator.next().value
+
+  b.suite(
+    `copycat.email(): 100_000 words`,
+    ...serializerNames.map(name => b.add(name, () => {
+      global.__bench_serializer = name
+      copycat.email(input)
+    })),
+    b.cycle(() => {
+      input = inputsIterator.next().value
+    }),
+    complete()
+  );
+})()
+
+;(() => {
+  b.suite(
+    `copycat.scramble(): nested json`,
+    ...serializerNames.map(name => b.add(name, () => {
+      global.__bench_serializer = name
+      copycat.scramble(nestedSample)
+    })),
+    complete()
+  )
+})()
 
 b.suite(
-  `copycat.email()`,
-  b.add('before', () => {
-    process.env.AFTER = false
+  `copycat.email(): nested json`,
+  ...serializerNames.map(name => b.add(name, () => {
+    global.__bench_serializer = name
     copycat.email(nestedSample)
-  }),
-  b.add('after', () => {
-    process.env.AFTER = true
-    copycat.email(nestedSample)
-  }),
+  })),
   complete()
 )
 
@@ -41,4 +71,19 @@ function complete() {
 
     console.log(JSON.stringify(summary, null, 2))
   })
+}
+
+function* generateWords(numWords) {
+  const generateInputSegment = fictional.join('', [copycat.times([3, 6], copycat.oneOf([
+    copycat.char.digit,
+    copycat.char.upper,
+    copycat.char.lower,
+  ]))])
+
+  const generateInput = id => copycat.times(id, numWords, id => fictional.tuple(id, [generateInputSegment, copycat.oneOf(['@', ',', '.', ' '])]).join('')).join('')
+  let i = -1
+
+  while (true) {
+    yield generateInput(++i)
+  }
 }
