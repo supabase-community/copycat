1b4c7bb6882aa2a63b64b98829200f0ecda42068
avallete <andrew@snaplet.dev>
2023-11-24 09:06:50 +0100

    feat: add options to phoneNumber method

---
+++ scripts/collisionsBasic.js
@@ -0,0 +1,26 @@
+const { v4: uuid } = require('uuid')
+
+const { TRANSFORMATIONS } = require('../dist/testutils')
+
+const METHOD = process.env.METHOD ? process.env.METHOD : 'phoneNumber'
+const MAX_N = +(process.env.MAX_N ?? 999999)
+
+function main() {
+  let firstColision = null
+  let colisions = 0
+  const colide = new Set()
+  for (let i = 0; i < MAX_N; i++) {
+    const result = TRANSFORMATIONS[METHOD](uuid())
+    if (colide.has(result)) {
+      colisions++
+      if (firstColision == null) {
+        firstColision = i
+      }
+    } else {
+      colide.add(result)
+    }
+  }
+  console.log(`firstColision: ${firstColision} colided ${colisions} times`)
+}
+
+main()
