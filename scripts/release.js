52b4abd5e447f16fc102895c63dbca777d7ab8d1
Justin van der Merwe <justnvdm@gmail.com>
2024-02-07 15:53:46 +0200

    chore: Change release approach for preview releases
    Instead of needing to maintain `next` and `main` branches, we now have a single `main` branch, and a new `yarn release` script to wrap
    up the release workflow. The script usage looks like this:
    
    ```
    yarn release <major|minor|patch>
    ```
    
    If the `major` is given as the version bump type, the script will also version release a preview version. For example, if the current version was 2.3.1, `yarn release major` would release two versions:
    * 3.0.0-preview.0 - the same as 2.3.1, except @snaplet/copycat/next would allow the user to preview 3.0.0
    * 3.0.0 - the actual major release
    ```
    
     ## Why the change?
    The nature of copycat (since it is meant to map to the same output for the same input) means that changes to the code frequently cause breaking changes (since the output often changes when the code changes). This happens often enough that we need a more streamlined way of releasing these breaking changes.
    
    The current approach is to maintain `next` and `main` branches and schedule breaking changes for a given date. This is problematic for a few reasons:
    * Needing to maintain two branches requires unnecessary branch juggling whenever one of the branches changes
    * Ideally users should be able to use major releases as soon as they would like to - without needing to change the way they use copycat and, without first waiting for a given date

---
+++ scripts/release.js
@@ -0,0 +1,112 @@
+#!/usr/bin/env node
+const semver = require('semver')
+const util = require('util')
+const cp = require('child_process')
+const fs = require('fs/promises');
+
+const doExec = util.promisify(cp.exec);
+
+const exec = async (cmd, opts) => {
+  console.log()
+  console.log(`$ ${cmd}`)
+
+  if (!process.env.DRY) {
+    return await doExec(cmd, { stdio: 'inherit', ...opts })
+  }
+
+  console.log()
+}
+
+const readPkgVersion = async () => {
+  const pkg = JSON.parse(await fs.readFile(require.resolve('../package.json')))
+  return pkg.version
+}
+
+const bumpVersion = async (b) => {
+  const a = await readPkgVersion()
+
+  if (!process.env.DRY) {
+    await exec(`npm pkg set version=${b}`)
+  }
+
+  console.log(`Version bumped from ${a} to ${b}`)
+}
+
+const computeVersion = async (bumpType) => {
+  const pkg = JSON.parse(await fs.readFile(require.resolve('../package.json')))
+  const currentVersion = pkg.version
+
+  let result
+
+  if (bumpType === 'preview') {
+   result = semver.inc(currentVersion, 'premajor', 'preview')
+  } else {
+   result = semver.inc(currentVersion, bumpType, 1)
+  }
+
+  return result
+}
+
+const publish = () => exec('yarn publish && git push && git push --tags')
+
+const ensureCleanGitState = () => {
+  console.log('Ensuring clean git state')
+  return exec('git clean -df && git checkout main && git pull --rebase && git push')
+}
+
+const releasePreviewVersion = async () => {
+  const previewVersion = await computeVersion('preview')
+  console.log(`Releasing preview version: ${previewVersion}`)
+
+  console.log('Ensuring dependencies are in sync with lockfile for preview version')
+  await exec(`git checkout v${await readPkgVersion()}`)
+  await exec('yarn')
+  await exec(`yarn build`)
+  await exec('git checkout -')
+
+  console.log('Building `@snaplet/copycat/next`')
+  await exec(`yarn build:next`)
+
+  await bumpVersion(previewVersion)
+  await publish()
+}
+
+const releaseVersion = async () => {
+  const version = await computeVersion('major')
+  console.log(`Releasing version ${version}`)
+
+  console.log('Ensuring dependencies are in sync with lockfile')
+  await exec('yarn')
+
+  await exec(`yarn build`)
+
+  await bumpVersion(version)
+  await publish()
+}
+
+const main = async () => {
+  const bumpType = process.argv[2]
+
+  if (process.argv.find((v) => v === '-h' || v === '--help' || v === 'help') || !bumpType) {
+    console.log(`
+yarn release: Script for releasing copycat
+
+If a major version is given, will also first release a preview version (where \`@snaplet/copycat/next\` previews the changes)
+
+Usage: yarn release <major|minor|patch>
+
+Example: yarn release major
+`)
+    return
+  }
+
+  await ensureCleanGitState()
+
+  if (bumpType === 'major') {
+    await releasePreviewVersion()
+  }
+
+  await releaseVersion(bumpType)
+}
+
+main()
